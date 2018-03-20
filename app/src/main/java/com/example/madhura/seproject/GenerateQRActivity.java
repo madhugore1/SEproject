52daa53d5a6ec444b157826343f91475d98c1e8b
Harsh <harshrjain98@gmail.com>
2018-03-20 22:42:27 +0530

    image storing

--- app/src/main/java/com/example/madhura/seproject/GenerateQRActivity.java
+++ app/src/main/java/com/example/madhura/seproject/GenerateQRActivity.java
@@ -1,22 +1,39 @@
 package com.example.madhura.seproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.sumimakito.awesomeqr.AwesomeQRCode;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class GenerateQRActivity extends AppCompatActivity {
 
@@ -28,6 +45,8 @@ public class GenerateQRActivity extends AppCompatActivity {
     private String TAG = GenerateQRActivity.class.getSimpleName();
     private int fare,amount,tickets;
     private String source,destination;
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +59,10 @@ public class GenerateQRActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         //get current user
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
         //get email of current user
         String email = user.getEmail();
 
@@ -67,6 +90,8 @@ public class GenerateQRActivity extends AppCompatActivity {
                 .size(800).margin(20)
                 .render();
         qrImage.setImageBitmap(qrCode1);
+        savebitmap(ticket_id,qrCode1);
+//        uploadImage(ticket_id,qrCode1);
 
         mUserReference.push().child("ticket_id").setValue(ticket_id);
 
@@ -74,5 +99,80 @@ public class GenerateQRActivity extends AppCompatActivity {
         mTicketReference = mFirebaseDatabase.getReference().child("tickets").child(ticket_id);
         mTicketReference.setValue(ticket);
 
+    }
+
+    private void savebitmap(String filename, Bitmap image) {
+        File pictureFile = getOutputMediaFile(filename);
+        if (pictureFile == null) {
+            Log.d(TAG,
+                    "Error creating media file, check storage permissions: ");// e.getMessage());
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+
+    private  File getOutputMediaFile(String filename){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Files");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName="MI_"+ timeStamp +".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + filename +".jpg");
+        return mediaFile;
+    }
+
+    private void uploadImage(String ticket_id, Bitmap qrcode) {
+
+//        final ProgressDialog progressDialog = new ProgressDialog(this);
+//        progressDialog.setTitle("Uploading...");
+//        progressDialog.show();
+//
+//        StorageReference ref = storageReference.child("tickets").child(ticket_id);
+//        ref.putFile()
+//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        progressDialog.dismiss();
+//                        Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        progressDialog.dismiss();
+//                        Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//                        double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+//                                .getTotalByteCount());
+//                        progressDialog.setMessage("Uploaded "+(int)progress+"%");
+//                    }
+//                });
     }
 }
